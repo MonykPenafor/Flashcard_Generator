98c8173d6cc7f973aa274d75caa3f77c499ee1ac
Monyk Paola Penafor <91429002+MonykPenafor@users.noreply.github.com>
2024-05-22 21:59:53 -0300

    add login form

---
+++ Flashcard_Generator/Login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="Login" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Flashcard_Generator.Login" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container">
+
+
+        <h3>LOGIN TO SEE YOUR FLASHCARDS</h3>
+
+        <asp:Label ID="lblMessage" runat="server" Text="" CssClass="error-message"/>
+
+        <div class="form-group login-form">
+            <label for="txtUsername">Username or Email:</label>
+            <asp:TextBox ID="txtUser" runat="server" CssClass="form-control"></asp:TextBox>
+
+            <label for="txtPassword" >Password:</label>
+            <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control" TextMode="Password"></asp:TextBox>
+        </div>
+
+
+
+        
+        <asp:Button ID="btnLogin" runat="server" Text="Login" CssClass="btn btn-primary btn-center" OnClick="login"/>
+
+
+
+<%--        <asp:Button ID="btnGenerator" runat="server" Text="Generator" CssClass="btn btn-primary btn-center" OnClick="goToGenerator" />--%>
+
+    </div>
+
+</asp:Content>
---
+++ Flashcard_Generator/Login.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Flashcard_Generator
+{
+	public partial class Login : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void login(object sender, EventArgs e)
+		{
+
+			string username_or_email = txtUser.Text;
+			string password = txtPassword.Text;
+
+
+			// checking fields are not null/empty
+			if (string.IsNullOrEmpty(username_or_email) || string.IsNullOrEmpty(password))
+			{
+				lblMessage.Text = "All fields are required!";
+				return;
+			}
+
+
+			// Hashing the password
+			string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+
+			string connectionString = ConfigurationManager.ConnectionStrings["FlashCardsDB"].ConnectionString;
+
+			using (SqlConnection con = new SqlConnection(connectionString))
+			{
+				//con.Open();
+				//string query = "INSERT INTO Users (Username, Email, Password_hash) VALUES (@Username, @Email, @Password)";
+				//using (SqlCommand cmd = new SqlCommand(query, con))
+				//{
+				//	cmd.Parameters.AddWithValue("@Username", username);
+				//	cmd.Parameters.AddWithValue("@Email", email);
+				//	//cmd.Parameters.AddWithValue("@Password", password);
+				//	cmd.Parameters.AddWithValue("@Password", hashedPassword); // Note the use of the hashed password
+
+
+				//	try
+				//	{
+				//		cmd.ExecuteNonQuery();
+				//		lblMessage.Text = "User registered successfully!";
+				//		Response.Redirect("FlashcardGenerator.aspx");
+				//	}
+				//	catch (SqlException ex)
+				//	{
+				//		// Check if the error is a violation of unique constraint
+				//		if (ex.Number == 2627) // Unique constraint error number
+				//		{
+				//			lblMessage.Text = "Username or Email Already registered!";
+				//		}
+				//		else
+				//		{
+
+				//			lblMessage.Text = $"Failed to register!: {ex}";
+				//		}
+				//	}
+				//	catch (Exception ex)
+				//	{
+
+				//		lblMessage.Text = $"Failed to register!: {ex}";
+				//	}
+				//}
+			}
+
+		}
+	}
+}
